0246d323eaf01a0321bde1265c0c4dae83f26b4c
Peter roe <peterroe@163.com>
2022-04-07 22:01:09 +0800

    three

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,11 +8,45 @@ const router = useRouter()
 const jumpTargetPage = (targetPath: string) => {
   router.push(targetPath)
 }
+const list = ref([
+  {
+    name: '发起签到',
+    icon: 'i-carbon-plane',
+    link: '',
+  },
+  {
+    name: '我要签到',
+    icon: 'i-carbon-plane',
+    link: '',
+  },
+  {
+    name: '发起记录',
+    icon: 'i-carbon-plane',
+    link: 'record',
+  },
+  {
+    name: '我要参与',
+    icon: 'i-carbon-plane',
+    link: 'join',
+  },
+  {
+    name: '我的空间',
+    icon: 'i-carbon-plane',
+    link: 'space',
+  },
+  {
+    name: '在线帮助',
+    icon: 'i-carbon-plane',
+    link: '',
+  },
+])
 </script>
 
 <template>
   <div grid="~ cols-2">
     <div
+      v-for="(item, index) in list"
+      :key="index"
       border="~ green-500"
       rounded="lg"
       w="30vw"
@@ -21,118 +55,15 @@ const jumpTargetPage = (targetPath: string) => {
       my="4"
       shadow="~ md gray-300"
       hover:bg="green-500/90"
-      hover:text="white"
       flex="~ col"
       justify="center"
+      @click="jumpTargetPage(item.link)"
     >
       <div h="12" text="center">
-        <div text="2xl" m="auto" class="i-carbon-plane" />
+        <div text="2xl" m="auto" :class="item.icon" />
       </div>
       <div text="center">
-        发起签到
-      </div>
-    </div>
-
-    <div
-      border="~ green-500"
-      rounded="lg"
-      w="30vw"
-      h="30vw"
-      mx="auto"
-      my="4"
-      shadow="~ md gray-300"
-      hover:bg="green-500/90"
-      hover:text="white"
-      flex="~ col"
-      justify="center"
-    >
-      <div h="12">
-        <div text="2xl" m="auto" class="i-carbon-plane" />
-      </div>
-      <div text="center">
-        我要签到
-      </div>
-    </div>
-
-    <div
-      border="~ green-500"
-      rounded="lg"
-      w="30vw"
-      h="30vw"
-      mx="auto"
-      my="4"
-      shadow="~ md gray-300"
-      hover:bg="green-500/90"
-      flex="~ col"
-      justify="center"
-      @click="jumpTargetPage('record')"
-    >
-      <div h="12" text="center">
-        <div text="2xl" m="auto" class="i-carbon-plane" />
-      </div>
-      <div text="center">
-        发起记录
-      </div>
-    </div>
-
-    <div
-      border="~ green-500"
-      rounded="lg"
-      w="30vw"
-      h="30vw"
-      mx="auto"
-      my="4"
-      shadow="~ md gray-300"
-      hover:bg="green-500/90"
-      flex="~ col"
-      justify="center"
-      @click="jumpTargetPage('join')"
-    >
-      <div h="12" text="center">
-        <div text="2xl" m="auto" class="i-carbon-plane" />
-      </div>
-      <div text="center">
-        我要参与
-      </div>
-    </div>
-
-    <div
-      border="~ green-500"
-      rounded="lg"
-      w="30vw"
-      h="30vw"
-      mx="auto"
-      my="4"
-      shadow="~ md gray-300"
-      hover:bg="green-500/90"
-      flex="~ col"
-      justify="center"
-    >
-      <div h="12" text="center">
-        <div text="2xl" m="auto" class="i-carbon-plane" />
-      </div>
-      <div text="center">
-        我的空间
-      </div>
-    </div>
-
-    <div
-      border="~ green-500"
-      rounded="lg"
-      w="30vw"
-      h="30vw"
-      mx="auto"
-      my="4"
-      shadow="~ md gray-300"
-      hover:bg="green-500/90"
-      flex="~ col"
-      justify="center"
-    >
-      <div h="12" text="center">
-        <div text="2xl" m="auto" class="i-carbon-plane" />
-      </div>
-      <div text="center">
-        在线帮助
+        {{ item.name }}
       </div>
     </div>
   </div>
