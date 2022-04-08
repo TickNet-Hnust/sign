0fb639fbcca3132e92bb1b4cd136156403a05fd9
thosewhoyoulove <90265074+thosewhoyoulove@users.noreply.github.com>
2022-04-08 15:58:23 +0800

    involve
    invovle pages

---
+++ src/pages/Space/involve/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="p-3 bg-gray-100">
+    <div class="flex justify-between header">
+      <span class="px-3 py-1 flex bg-white rounded">
+        <span class="mr-3 text-2xl" style="color: rgb(156, 163, 175)"><van-icon name="search" /></span>
+        <input placeholder="请输入要搜索的空间">
+      </span>
+      <span class="text-xl">
+        <span class="plus_btn font-bold px-0.2"><van-icon name="edit" /></span>
+      </span>
+    </div>
+    <div class="mt-8 border border-gray-200 bg-white rounded py-3 px-5">
+      <van-list
+        v-model:loading="loading"
+        :finished="finished"
+        finished-text="没有更多了"
+      >
+        <ul v-for="item in space_list" :key="item" class="list_item mt-3">
+          <div class="text-left font-bold">
+            {{ item.title }}
+          </div>
+          <div class="text-left text-xs mt-1 mb-1 item_time">
+            {{ item.time }}
+          </div>
+        </ul>
+      </van-list>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+const loading = ref(false)
+const fininshed = ref(true)
+const space_list = ref([
+  {
+    title: '班级会议',
+    time: '2022-01-03 17:34:38',
+  },
+  {
+    title: 'ticknet会议',
+    time: '2022-01-03 17:34:38',
+  },
+  {
+    title: '班级会议',
+    time: '2022-01-03 17:34:39',
+  },
+])
+</script>
+
+<style scoped>
+.van-icon {
+  font-weight: 700;
+}
+.header {
+  align-items: center;
+}
+.plus_btn {
+  color: rgb(89, 180, 118);
+  border: 3px solid rgb(89, 180, 118);
+  border-radius: 0.2em;
+}
+.list_item {
+  border-bottom: 1px solid rgb(222, 222, 222);
+}
+.item_time {
+  color: #999;
+}
+</style>
---
+++ src/pages/Space/involve/involve_details/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div bg="gray-100" p="2">
+    <div bg="white" p="1" display="flex" class=" border  border-gray-300" rounded="default">
+      <div><img class="w-45px h-45px" src="https://www.bing.com/th?id=OIP.Fz_jwVT_HbyhaQ83m7h_IAHaE7&w=306&h=204&c=8&rs=1&qlt=90&o=6&dpr=1.25&pid=3.1&rm=2" alt=""></div>
+      <div text="left">
+        <span text="sm" mx="3" py="1">19级年级大会</span><br>
+        <span text="sm" mx="3" py="1">成员：{{ member_list.length }}</span>
+      </div>
+    </div>
+    <div text="left" ml="2" my="3" font="extralight">
+      成员列表
+    </div>
+    <div mt="3" rounded="default" border="~ solid gray-200 3" bg="white">
+      <ul v-for="item in member_list" :key="item" p="3" m="1" class="border-b-2">
+        <div class="flex justify-between">
+          <span>
+            <span font="200">{{ item.id }}</span>
+            <span font="medium" text="md" class="ml-10">{{ item.name }}</span>
+          </span>
+          <span v-if="item.status" text="xs" class="p-1 border rounded border-gray-300">{{ item.status }}</span>
+        </div>
+      </ul>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+const member_list = ref([
+  {
+    id: '1905040117',
+    name: '曹俊',
+    status: '管理员',
+  },
+  {
+    id: '1905040110',
+    name: '曹帅',
+    status: '',
+  },
+])
+</script>
+<style scoped>
+
+</style>
