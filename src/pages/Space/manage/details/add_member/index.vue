f6ba6bd327469ae1091187a5a360e99994573814
7UP-hue <2176859595@qq.com>
2022-04-09 15:22:39 +0800

    delete style

--- src/pages/Space/manage/details/add_member/index.vue
+++ src/pages/Space/manage/details/add_member/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-gray-100 p-5">
-    <div class="tips text-xs p-2">签到发起人可以通过生成二维码来追加成员</div>
+    <div class="text-xs p-2 bg-hex-E0FAFB text-hex-003399 border border-hex-A6DEFB">签到发起人可以通过生成二维码来追加成员</div>
     <div
       class="mt-6 p-4 font-semibold rounded bg-gray-250 border border-gray-300"
     >
@@ -9,7 +9,7 @@
       </div>
       <div>
         <img
-          class="erweima"
+          class="mt-4 mx-auto w-240px"
           src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2Ff26f5ed0d000c3413c9a4250f2c22bcb86a9494e2b2a-Gw0bYT_fw658&refer=http%3A%2F%2Fhbimg.b0.upaiyun.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1649495467&t=ce9ebbe0f7270b03850a7a05fa695c33"
         />
       </div>
@@ -17,13 +17,5 @@
   </div>
 </template>
 <style scoped>
-.erweima {
-  max-width: 70%;
-  margin: 1em auto;
-}
-.tips {
-  background-color: rgb(224, 250, 251);
-  color: rgb(0, 51, 153);
-  border: 1px solid rgb(166, 222, 251);
-}
+
 </style>
