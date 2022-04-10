17d06be7e91b9617f81e974356fc7fe5d367bd0e
Resurgam <1791614500@qq.com>
2022-04-10 19:19:13 +0800

    update style

--- src/pages/join/sign/index.vue
+++ src/pages/join/sign/index.vue
@@ -1,15 +1,57 @@
+<script setup lang="ts">
+import { Dialog } from 'vant'
+
+interface SignData{
+  tittleText: string|undefined
+  userName: string
+  signCode: string // 用户输入的签到码
+  code: string // 正确的签到码
+  isCheckCode: boolean // 比较用户输入的签到码是否正确
+}
+
+const pattern = /^\d{4}$/ // 用户输入的签到码正则匹配规则
+const signData: SignData = reactive({
+  userName: '秦豪远',
+  signCode: '',
+  code: '2000',
+  isCheckCode: false,
+  tittleText: computed(() => {
+    if (!pattern.test(signData.signCode))
+      return '请输入4位有效的签到码*^_^*'
+    else if (pattern.test(signData.signCode) && signData.code !== signData.signCode)
+      return '你的附近好像没有该签到码噢'
+    else if (pattern.test(signData.signCode) && signData.code === signData.signCode)
+      return signData.userName
+  }),
+
+})
+
+const checkCode = function() {
+  if (signData.code === signData.signCode) {
+    signData.isCheckCode = true
+  }
+  else {
+    Dialog.alert({
+      message: '签到码错误，请重新输入！！！',
+    }).then(() => {
+      // on close
+    })
+  }
+}
+
+</script>
 <template>
   <div class="bg-true-gray-50 py-5 px-4">
-    <div class="bg-cyan-100  rounded-sm border-1 border-blue-300 p-2 text-sm" style="background-color:#DFF7FA;color:#003399">
-      请输入四位4效签到码
+    <div class="bg-cyan-100  rounded-sm border-1 border-blue-300 p-2 text-xs" style="background-color:#DFF7FA;color:#003399">
+      {{ signData.tittleText }}
     </div>
     <div class="my-6 flex justify-center">
       <van-cell-group inset class="border rounded-0.5 w-25" style="border-color:#999999">
-        <van-field placeholder="输入签到码" input-align="center" class="p-1" />
+        <van-field v-model="signData.signCode" placeholder="输入签到码" input-align="center" class="p-1" maxlength="4" />
       </van-cell-group>
     </div>
     <div class="flex justify-center">
-      <van-button plain type="primary" color="green" class="w-30">
+      <van-button plain type="primary" class="w-30 font-600" :color="pattern.test(signData.signCode) && signData.code === signData.signCode? '#41AA62':'#99CDAC'" @click="checkCode()">
         确认签到
       </van-button>
     </div>
