a86a53d60f2c03ff557c489ea55b6342e52e1409
Peter roe <peterroe@163.com>
2022-04-15 13:10:38 +0800

    delete useless layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -12,8 +12,5 @@ const title = router.currentRoute.value.meta.title || '默认标题'
       @click-left="router.back()"
     />
     <router-view />
-    <div class="mt-5 mx-auto text-center opacity-25 text-sm">
-      [By TickNets]
-    </div>
   </main>
 </template>
--- src/pages/join/index.vue
+++ src/pages/join/index.vue
@@ -2,13 +2,13 @@
   <div class="bg-gray-500/8 p-3">
     <div class="top flex bg-white rounded">
       <span class="flex-2">
-       <van-field
-         v-model="value"
-         placeholder="请输入要搜索的记录"
-         left-icon="search"
-         />
+        <van-field
+          v-model="value"
+          placeholder="请输入要搜索的记录"
+          left-icon="search"
+        />
       </span>
-      <span 
+      <span
         class="
           flex-1
           text-13px
@@ -18,12 +18,12 @@
           justify-center"
       >时间<van-icon name="arrow-down" /></span>
       <span
-        class="flex items-center justify-end text-13px text-hex-666 flex-1 pr-3"
         id="showSelect"
+        class="flex items-center justify-end text-13px text-hex-666 flex-1 pr-3"
       >
         <van-dropdown-menu active-color="#1989fa">
           <van-dropdown-item v-model="choValue" :options="choOptions" />
-        </van-dropdown-menu> 
+        </van-dropdown-menu>
         <!-- 类型筛选<van-icon name="filter-o" /> -->
       </span>
     </div>
@@ -41,6 +41,7 @@
           style="background-color: #fff; position: relative"
         >
           <div
+            v-if="!item.ended"
             class="h-0 w-0"
             style="
                border: 11px solid transparent;
@@ -49,8 +50,7 @@
                position: absolute;
                top: 0;
                left: 0"
-            v-if="!item.ended"
-          ></div>
+          />
           <div
             class="w-54px h-54px"
             style="
@@ -58,8 +58,10 @@
               top: 0;
               right: 84px;
               background: url(../../../public/join.png);
-              background-size: 100%"></div>
+              background-size: 100%"
+          />
           <div
+            v-if="item.ended"
             class="h-0 w-0"
             style="
                border: 11px solid transparent;
@@ -68,13 +70,12 @@
                position: absolute;
                top: 0;
                left: 0"
-            v-if="item.ended"
-          ></div>
+          />
           <div style="display: flex; justify-content: space-between">
             <span class="text-base font-semibold">{{ item.title }}</span>
             <span class="text-xs">
-              <span class="bg-hex-41BD62 text-white px-2 py-1 rounded" v-if="!item.ended">{{item.type}}中</span>
-              <span class="bg-hex-C9C9C9 text-hex-7E7E7E px-2 py-1 rounded" v-if="item.ended">{{item.type}}中</span>
+              <span v-if="!item.ended" class="bg-hex-41BD62 text-white px-2 py-1 rounded">{{ item.type }}中</span>
+              <span v-if="item.ended" class="bg-hex-C9C9C9 text-hex-7E7E7E px-2 py-1 rounded">{{ item.type }}中</span>
             </span>
           </div>
           <div class="text-xs text-gray-400 text-left mt-3">
@@ -94,7 +95,7 @@ meta:
 
 <script setup lang="ts">
 const isShow = ref(false)
-const columns = ref(['全部','签到','投票','抽签'])
+const columns = ref(['全部', '签到', '投票', '抽签'])
 const choValue = ref('cho')
 const choOptions = ref([
   { text: '类型筛选', value: 'cho' },
@@ -105,73 +106,73 @@ const choOptions = ref([
 ])
 const list = ref([
   {
-    title: "冬奥会2022年什么时候举办",
-    status: "抽签中",
-    time: "2022-01-03 17:34:38",
-    user: "张智豪",
-    type: "抽签",
+    title: '冬奥会2022年什么时候举办',
+    status: '抽签中',
+    time: '2022-01-03 17:34:38',
+    user: '张智豪',
+    type: '抽签',
     ended: false,
-    color: "rgb(118,211,246)",
-    space: "班级会议",
+    color: 'rgb(118,211,246)',
+    space: '班级会议',
     unford_show: false,
-    unford_icon: "arrow-down",
+    unford_icon: 'arrow-down',
   },
   {
-    title: "冬奥会2022年什么时候举办",
-    status: "投票中",
-    time: "2022-01-03 17:34:38",
-    user: "张智豪",
-    type: "投票",
+    title: '冬奥会2022年什么时候举办',
+    status: '投票中',
+    time: '2022-01-03 17:34:38',
+    user: '张智豪',
+    type: '投票',
     ended: false,
-    color: "rgb(118,211,246)",
-    space: "班级会议",
+    color: 'rgb(118,211,246)',
+    space: '班级会议',
     unford_show: false,
-    unford_icon: "arrow-down",
+    unford_icon: 'arrow-down',
   },
   {
-    title: "签到",
-    status: "签到中",
-    time: "2022-01-03 17:34:38",
-    user: "张智豪",
-    type: "签到",
+    title: '签到',
+    status: '签到中',
+    time: '2022-01-03 17:34:38',
+    user: '张智豪',
+    type: '签到',
     ended: false,
-    color: "rgb(118,211,246)",
-    space: "班级会议",
+    color: 'rgb(118,211,246)',
+    space: '班级会议',
     unford_show: false,
-    unford_icon: "arrow-down",
+    unford_icon: 'arrow-down',
   },
   {
-    title: "签到",
-    status: "已结束",
-    time: "2022-01-03 17:34:38",
-    user: "张智豪",
-    type: "签到",
+    title: '签到',
+    status: '已结束',
+    time: '2022-01-03 17:34:38',
+    user: '张智豪',
+    type: '签到',
     ended: true,
-    color: "rgb(201,201,201)",
-    space: "班级会议",
+    color: 'rgb(201,201,201)',
+    space: '班级会议',
     unford_show: false,
-    unford_icon: "arrow-down",
+    unford_icon: 'arrow-down',
   },
   {
-    title: "签到",
-    status: "已结束",
-    time: "2022-01-03 17:34:38",
-    user: "张智豪",
-    type: "签到",
+    title: '签到',
+    status: '已结束',
+    time: '2022-01-03 17:34:38',
+    user: '张智豪',
+    type: '签到',
     ended: true,
-    color: "rgb(201,201,201)",
-    space: "班级会议",
+    color: 'rgb(201,201,201)',
+    space: '班级会议',
     unford_show: false,
-    unford_icon: "arrow-down",
-  }
-]);
-const loading = ref(false);
-const finished = ref(false);
+    unford_icon: 'arrow-down',
+  },
+])
+const loading = ref(false)
+const finished = ref(false)
 const onload = () => {
   // setTimeout(() => {
   //     loading.value = false;
   // }, 1000);
-};
+}
 
 </script>
 <style>
