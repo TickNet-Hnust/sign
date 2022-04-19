2b29a6225c4776d5582c944f2e8139cdcb0f42f0
7UP-hue <2176859595@qq.com>
2022-04-19 20:18:06 +0800

    add jumpLink

--- src/pages/Space/manage/details/index.vue
+++ src/pages/Space/manage/details/index.vue
@@ -8,26 +8,32 @@ const details_list = reactive([
   {
     title: '发起签到',
     icon: 'i-carbon-3d-cursor-alt',
+    link: 'checkIn'
   },
   {
     title: '发起投票',
     icon: 'i-carbon-receipt',
+    link: 'vote'
   },
   {
     title: '发起抽签',
     icon: 'i-carbon-align-box-middle-center',
+    link: 'draw'
   },
   {
     title: '发起点名',
     icon: 'i-carbon-user-certification',
+    link: 'call_the_roll'
   },
   {
     title: '成员追加',
     icon: 'i-carbon-user-follow',
+    link: 'add_member'
   },
   {
     title: '积分',
     icon: 'i-carbon-report',
+    link: 'scorecard'
   },
 ])
 const member_list = reactive([
@@ -40,20 +46,23 @@ const member_list = reactive([
     member: '曹shuaige',
   },
 ])
-
+const router = useRouter()
 const finished = ref(true)
+const jumpPage = (path) => {
+  router.push('details/'+path)
+}
 </script>
 <template>
   <div class="bg-hex-F6F7F9 p-3">
-    <div class="flex bg-hex-fff rounded">
-      <div class="flex-col flex ml-8">
-        <div class="mt-2">
+    <div class="flex bg-hex-fff rounded justify-between px-5 pt-5">
+      <div class="flex-col flex">
+        <div>
           <span class="mt-6 mr-4 text-md">班级会议</span>
           <span><van-icon name="edit" /></span>
         </div>
-        <span class="mt-7 text-xs text-left">成员：{{ number }}</span>
+        <span class="mt-3 text-xs text-left">成员：{{ number }}</span>
       </div>
-      <div class="rounded flex-1 mt-7 ml-7">
+      <div class="rounded">
         <van-button type="danger" class="rounded" size="small">
           删除空间
         </van-button>
@@ -61,14 +70,15 @@ const finished = ref(true)
     </div>
     <van-tabs v-model:active="active" color="rgb(0,51,255)">
       <van-tab title="概况">
-        <div class="mt-5 flex flex-wrap justify-around">
+        <div class="mt-3 rounded flex flex-wrap justify-around bg-white">
           <div
             v-for="item in details_list"
             :key="item"
-            class="mt-5 p-3 inline-block ml-3 border border-hex-41be62 text-hex-999 rounded w-30vw h-30vw"
+            class="my-5 p-3 mx-5 inline-block border border-hex-41be62 text-hex-999 rounded w-25vw h-25vw"
             shadow="~ md gray-400/60"
+            @click="jumpPage(item.link)"
           >
-            <div class="boxIcon mt-2" text="2xl" color="green-600" m="auto" :class="item.icon" />
+            <div class="mt-2" text="2xl" color="green-600" m="auto" :class="item.icon" />
             <div class="mt-3 text-14px">
               {{ item.title }}
             </div>
@@ -94,3 +104,8 @@ const finished = ref(true)
     </van-tabs>
   </div>
 </template>
+<route lang="yaml">
+meta:
+  layout: default
+  title: 空间详情
+</route>
