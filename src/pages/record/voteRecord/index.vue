c57ab8e5c2b5b3a0cc96fb0aded0f678588783cb
7UP-hue <2176859595@qq.com>
2022-05-14 18:00:21 +0800

    change

--- src/pages/record/voteRecord/index.vue
+++ src/pages/record/voteRecord/index.vue
@@ -1,52 +1,21 @@
 <script setup lang="ts">
 import { getDetailVote, voteStuList } from '~/api/record/voteRecord'
+import signList from '~/components/recordList/stuList.vue'
 interface Choer {
   createUserId: String //学号
   createUserName: String //姓名
   createTime: String //投票
 }
 interface ChoList {
-  title: String //选项名
+  choOption: String //选项名
   count: Number //票数
   isShow: Boolean //是否展开该列表
   choer: Array<Choer> //选中这个选项的人
 }
-// 未投票列表
-const no_list = reactive([
-  {
-    num: "1905020118",
-    name: "张三",
-    status: "",
-  },
-  {
-    num: "1905020118",
-    name: "张三",
-    status: "",
-  },
-  {
-    num: "1905020118",
-    name: "张三",
-    status: "",
-  },
-  {
-    num: "1905020118",
-    name: "张三",
-    status: "请假",
-  },
-  {
-    num: "1905020118",
-    name: "张三",
-    status: "",
-  },
-  {
-    num: "1905020118",
-    name: "张三",
-    status: "",
-  },
-]);
 // 已投票列表
 const cho_list: Array<ChoList> = reactive([])
 const voteCount = ref() // 已抽签人数
+// 展示不同子列表
 const changeShow = (item: any) => {
   if(!item.isShow) {
     item.isShow = true;
@@ -76,11 +45,12 @@ onMounted(() => {
         voteId: voteId,
         pageNum: 1,
         pageSize: 10,
-        optionName: ''
+        optionName: '',
+        attend: 1
       })
       detailRecord.optionsList.forEach((item,index) => {
         let fItem:ChoList = {
-          title: item,
+          choOption: item,
           isShow: false,
           count: 0,
           choer: []
@@ -98,6 +68,11 @@ onMounted(() => {
     console.log(err)
   })
 })
+// 获取子组件传过来的未投票人数
+const notSign = ref(0)
+const getTotal = (total: any) => {
+  notSign.value = total
+}
 </script>
 
 <template>
@@ -129,7 +104,7 @@ onMounted(() => {
               <div class="bg-white mt-3 rounded flex justify-between p-3 text-14px">
                  <div>
                     <span>投票：</span>
-                    <span>{{item.title}}</span>
+                    <span>{{item.choOption}}</span>
                   </div>
                   <div>
                     <span class="bg-hex-30B648 rounded-lg text-white text-xs py-0.5 px-2 mr-3">{{item.count}} 票</span>
@@ -163,26 +138,9 @@ onMounted(() => {
         <van-tab>
           <template #title>
             <span class="text-sm">未投票</span>
-            <span class="bg-hex-30B648 rounded-lg text-white text-xs py-0.5 px-2 ml-2">7人</span>
+            <span class="bg-hex-30B648 rounded-lg text-white text-xs py-0.5 px-2 ml-2">{{notSign}}人</span>
           </template>
-          <div class="mt-3 bg-white rounded border border-hex-ccc px-5 py-2">
-            <van-list>
-              <ul class="flex justify-around border-b border-hex-ccc py-3 text-sm">
-                <span class="flex-1">学号/工号<van-icon name="sort" /></span>
-                <span class="flex-1">姓名</span>
-                <span class="flex-1">状态</span
-                >
-              </ul>
-              <ul class="flex justify-around border-b border-hex-ccc py-3 text-sm" v-for="item in no_list" :key="item">
-                <span class="flex-1">{{ item.num }}</span>
-                <span class="flex-1">{{ item.name }}</span>
-                <span class="flex-1" style="color: rgb(0,102,204)">{{ item.status }}</span>
-              </ul>
-              <ul class="text-x py-3">
-                没有更多了
-              </ul>
-            </van-list>
-          </div>
+          <stu-list @getTotal="getTotal" action="vote" :activityId="voteId" attend="0"></stu-list>
         </van-tab>
       </van-tabs>
     </div>
