7f6ee1d22468b1d2cc9a2c342cd15a6563f48fde
thosewhoyoulove <2406001659@qq.com>
2022-06-04 11:26:23 +0800

    下拉刷新空间列表，code校验

--- src/pages/Space/index.vue
+++ src/pages/Space/index.vue
@@ -7,7 +7,7 @@ export default {
 </script>
 
 <script setup lang="ts">
-import { Notify } from 'vant'
+import { Notify, Toast } from 'vant'
 import { addAFTFSpace, signSpaceList } from '~/api/mySpace/index'
 import { getCurrentInstance } from 'vue'
 const { eventHub } = getCurrentInstance()?.proxy
@@ -197,8 +197,18 @@ const nameRules = [
     message: '请输入空间名称',
   },
 ]
+//下拉刷新空间列表
+const refreshing = ref(false)
+const onRefresh = () =>{
+  setTimeout(() =>{
+    getsignSpaceList()
+    Toast('刷新成功');
+    refreshing.value = false;
+  },1000)
+}
 
 const isShow = ref(false)//控制回到顶部的箭头的展示
+//点击按钮回到顶部
 const toTop = () => {
     window.scrollTo({
     top: 0,
@@ -323,7 +333,7 @@ onUnmounted (()=>{
         <svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--ic color-hex-0C75A1 b-hex-0C75A1" width="25" height="30" @click="toTop" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M11.41 2.87a.99.99 0 0 1 1.18 0c1.22.88 3.91 3.59 3.91 10.13c0 2.16-.78 4.76-1.36 6.35c-.14.39-.51.65-.93.65H9.8c-.42 0-.8-.26-.94-.65C8.28 17.76 7.5 15.16 7.5 13c0-6.54 2.69-9.25 3.91-10.13zM14 11c0-1.1-.9-2-2-2s-2 .9-2 2s.9 2 2 2s2-.9 2-2zm-6.31 9.52c-.48-1.23-1.52-4.17-1.67-6.87l-1.13.75c-.56.38-.89 1-.89 1.67v4.45c0 .71.71 1.19 1.37.93l2.32-.93zm12.31 0v-4.45c0-.67-.33-1.29-.89-1.66l-1.13-.75c-.15 2.69-1.2 5.64-1.67 6.87l2.32.93c.66.25 1.37-.23 1.37-.94z"></path></svg>
       </van-sticky>
       </div>
-      
+      <van-pull-refresh v-model="refreshing" @refresh="onRefresh">
       <van-list>
         <van-loading
           v-if="showLoading"
@@ -353,9 +363,8 @@ onUnmounted (()=>{
         <div v-if=" spaceList.length===0 " class="text-sm text-hex-aaa mb-5 px-5 mt-10">
           空空如也~
         </div>
-        <van-list />
       </van-list>
-      
+      </van-pull-refresh>
     </div>
   </div>
 </template>
